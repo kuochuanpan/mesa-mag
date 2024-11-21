dd53dd47a20e736bd8ca84984606e9883d14c80c
K.-C. Pan <kuochuan.pan@gmail.com>
2024-11-21 15:10:57 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '24.11.21'
+DESCRIPTION = 'Convert MESA data into magnitudes.'
+LONG_DESCRIPTION = 'Convert MESA data into magnitudes.'
+
+setup(
+    name='mesa-mag',
+    version=VERSION,
+    author="Kuo-Chuan Pan",
+    author_email="<kuochuan.pan@gapp.nthu.edu.tw>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    license='MIT',
+    packages=find_packages(),
+    package_data={'mesa_mag': ['filters/*.dat']},
+    install_requires=['numpy',],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],  
+)
